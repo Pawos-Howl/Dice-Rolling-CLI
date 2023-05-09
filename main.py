fb273749db1b9197964ff2ed9cd351f81f93e0d8
Pawos-Howl <pawoshowl@gmail.com>
2023-05-09 14:11:59 -0700

    Minor Changes

--- main.py
+++ main.py
@@ -10,25 +10,11 @@ while True:
         # ^ okay i get that but these are just too long and look like eachother.
         # I recommend changing them.
         barkbarkkkAwrooo = roller.mainRoll(rollValue)
-        if barkbarkkkAwrooo[0:2] == "()": # Just do `if "()" in varname:`
+        if "()" in barkbarkkkAwrooo:
             woofbarkkkAwroooAWOOOO = barkbarkkkAwrooo[2:] #FYI!!! THIS IS THE EMIPHERIAL CODE FOR DISCORD BOTS!!!!
         else:
             woofbarkkkAwroooAWOOOO = barkbarkkkAwrooo
         print(woofbarkkkAwroooAWOOOO)
-
-        # Testing Repeated Contains Function
-        # print("What are you looking for:")
-        # secondInput = str(input())
-        # print(f'The string \"{rollValue}\" has \"{secondInput}\" returns: {roller.repeatedContains(rollValue, secondInput)}')
-
-        # Testing Contains Function
-        # print("What are you looking for:")
-        # secondInput = str(input())
-        # print(f'The string \"{rollValue}\" has \"{secondInput}\" returns: {roller.contains(rollValue, secondInput)}')
-
-        # The following lines are test of the valid Input thing
-        # usedCharacters = ['0','1','2','3','4','5','6','7','8','9','d','*','+', '-', ' ']
-        # print(f'The result of \"{rollValue}\" running the check is: {roller.validInput(rollValue, usedCharacters)}')
 
     except KeyboardInterrupt:
         exit("The program is closing...")
